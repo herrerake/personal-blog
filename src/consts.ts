8e30afcd4c26c5bb5e403d2e973ab70154609cdd
Ibrahim John <65255510+IbrahimOwner2020@users.noreply.github.com>
2024-03-23 22:18:28 +0300

    Layout improve (#3)
    * fix: update header and footer to match Figma design specs
    
    * fix: update card wrapper and made it reusable
    
    * add prettier formatter plugin for astro
    
    * update: layout file naming

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Herrerake';
-export const SITE_DESCRIPTION = 'Life and times of Kevin Herrera';
+export const SITE_TITLE = "Herrerake";
+export const SITE_DESCRIPTION = "Life and times of Kevin Herrera";
