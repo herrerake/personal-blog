0a3c9cf3d8521bbe7bc8fe012c7aaa6288713491
Ibrahim John <65255510+IbrahimOwner2020@users.noreply.github.com>
2024-04-13 00:22:41 +0300

    Portfolio (#7)
    * feat: add portfolio landing
    
    * feat: testing view transition
    
    * feat: single portfolio page
    
    * fix: layout alignment
    
    * Resolve comments:
    - Adding comment to the prose component
    - Refactor the viewTransition component to the whole app
    
    * Resolve comments:
    -Home page layout shift
    -Card height

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,4 +18,21 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const portfolio = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    featured: z.boolean(),
+  }),
+});
+
+export const collections = { blog, portfolio };
